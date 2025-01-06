5f3b861c6499c02bf205e340cc54731a318adac2
zzlava <zzlava@gmail.com>
2025-01-06 13:51:22 +0800

    fix: update Sanity structure builder import path

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,7 +1,12 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/desk'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title('内容')
+    .items([
+      S.listItem()
+        .title('网站')
+        .schemaType('site')
+        .child(S.documentList().title('网站').filter('_type == "site"')),
+    ])
