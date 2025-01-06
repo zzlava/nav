6a0e8e8ec1c701cc19878910401874366c49fbc7
zzlava <zzlava@gmail.com>
2025-01-06 10:00:01 +0800

    fix: update webpack config for chrome-aws-lambda

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // 排除 source map 文件
+    config.module.rules.push({
+      test: /\.map$/,
+      use: 'ignore-loader'
+    });
+
+    // 处理 chrome-aws-lambda
+    if (isServer) {
+      config.externals.push('chrome-aws-lambda');
+    }
+
+    return config;
+  },
 }
 
 module.exports = nextConfig 
