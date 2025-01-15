acb6ba9521d97cf5abf09d62f7d47a865ee374b6
zzlava <zzlava@gmail.com>
2025-01-15 10:09:19 +0800

    fix: 更新 Sanity 客户端配置

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,11 +3,11 @@ import imageUrlBuilder from '@sanity/image-url'
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  useCdn: true,
   apiVersion: '2024-01-06',
-  token: process.env.SANITY_API_TOKEN
+  token: process.env.SANITY_API_TOKEN,
+  useCdn: false,
 })
 
 const builder = imageUrlBuilder(client)
